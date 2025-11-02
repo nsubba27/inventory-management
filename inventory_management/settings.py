961845653003a1aa2307d167cb5bbb3ca622dfa5
Nishan Subba <236131907+nsubba27@users.noreply.github.com>
2025-11-02 15:10:22 -0500

    secured access

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+&3s7=x8cjky59yu#_ps07csj8e4$wj&sfd#tdhfwjdueam98#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
